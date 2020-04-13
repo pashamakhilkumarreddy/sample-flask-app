a91b739e31f2de64ee3df3ae31f3cb653306b7a5
pashamakhilkumarreddy <akhil.reddy@frequence.com>
2020-04-13 21:06:35 +0530

    Save users to a .csv file and add additional routes

--- src/app.py
+++ src/app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import os
+import smtplib
+import csv
 
 app = Flask(__name__)
+
+# app.config.from_pyfile('settings.py')
+
+students=[]
+
+EMAIL = os.getenv("EMAIL")
+
+PASSWORD = os.getenv('PASSWORD')
+
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
 
 @app.route("/")
 def index():
     name = request.args.get('name', 'world')
     return render_template('home/index.html', name=name)
+
+@app.route("/login")
+def loginGet():
+    return render_template('auth/login.html')
 
 @app.route("/register")
 def registerGet():
@@ -13,8 +32,30 @@ def registerGet():
 
 @app.route("/register", methods=['POST'])
 def registerPost():
-    email = request.form.get("email")
-    password = request.form.get("password")
-    if not email or not password:
-        return 'Fail'
-    return render_template('success.html')
+    name = request.form.get("name", None)
+    email = request.form.get("email", None)
+    password = request.form.get("password", None)
+    if not name or not email or not password:
+        return render_template('messages/errors/failure.html')
+    student = dict()
+    student['name'] = name
+    student['email'] = email
+    student['password'] = password
+    students.append(student)
+    file = open("users.csv", "a")
+    writer = csv.writer(file)
+    writer.writerow((name, email, password))
+    file.close()
+    message = "You are successfully registered"
+    server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
+    server.starttls()
+    server.login(EMAIL, PASSWORD)
+    server.sendmail(EMAIL, email, message)
+    return redirect('/users')
+
+@app.route("/users")
+def list_users():
+    with open('users.csv', 'r') as file:
+        reader = csv.reader(file)
+        students_csv = list(reader)
+    return render_template('users/list_users.html', students=students_csv)
